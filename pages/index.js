4c8db28b2a731ebf4adac5d064660a8941c407e6
Thomas Wu <62056970+thomaswcy@users.noreply.github.com>
2023-02-28 22:36:13 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,174 +1,175 @@
 import Meta from "@hackclub/meta";
-import Flag from "../components/flag";
 import Head from "next/head";
-import Layout from "../components/layout";
-import Foreword from "../components/forward";
-import Hackathon from "../components/hackathon";
-import Expectations from "../components/expectations";
-import Register from "../components/register";
-import Glossary from "../components/glossary";
-import ScrollHint from "../components/scroll-hint";
 
-import { Heading, Box, Text, Flex, Grid } from "theme-ui";
+import { Heading, Box, Text, Flex, Grid, ThemeProvider } from "theme-ui";
 import { Nav } from "../components/nav";
 import theme from "../lib/theme";
+/** @jsxImportSource theme-ui */
 
-export default function Main() {
+import { ThemeProvider } from 'theme-ui';
+import theme from './theme';
+
+function App() {
   return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width,initial-scale=1.0" />
-      </Head>
-      <Nav />
-      <Meta
-        as={Head}
-        name="Reboot"
-        title="Reboot - First hackathon in China since 2019"
-        description="In Shanghai, this April, 50+ teenage hackers will reboot hackathons in China." // page description
-        color={`#FF4794`}
-      />
-      <h1 style={{display: 'none'}}>
-        Reboot - high school hackathon in Shanghai.
-      </h1>
-      <Grid
-        sx={{
-          height: "100vh",
-          backgroundImage: `linear-gradient(0deg, rgba(0, 0, 0, 0.5) 2.96%, rgba(0, 0, 0,0.9) 100%), url(/assemble.jpg)`,
-          backgroundSize: "cover",
-          backgroundPosition: "bottom",
-        }}
-      >
-        <Flex sx={{ height: "100%", flexDirection: 'column' }}>
-          <Grid
-            columns={[1, 2]}
-            gap={5}
-            py={[3, 5]}
-            px={[3, 5]}
-            sx={{ width: "100%" }}
-          >
-            <Box
-              sx={{
-                textAlign: "center",
-                width: ["100%", "min-content"],
-                mt: [4, 0],
-                fontFamily: `"IBM Plex Mono"!important`,
-                mb: [-3, 0],
-              }}
-            >
-              <Flex>
-                <Box
-                  sx={{
-                    height: "18.42px",
-                    width: "23.61px",
-                    borderTop: "1px solid",
-                    borderLeft: "1px solid",
-                    borderColor: "rgba(175, 175, 175, 1)",
-                  }}
-                />
-                <Box sx={{ flexGrow: 1 }} />
-                <Box
-                  sx={{
-                    height: "18.42px",
-                    width: "23.61px",
-                    borderTop: "1px solid",
-                    borderRight: "1px solid",
-                    borderColor: "rgba(175, 175, 175, 1)",
-                  }}
-                />
-              </Flex>
-              <Box
-                sx={{
-                  fontSize: [2, 4],
-                  mb: 3,
-                  px: 4,
-                  zIndex: 999,
-                  position: "relative",
-                }}
-              >
-                HACKERS,
-              </Box>
-              <Heading
-                as="div"
-                sx={{
-                  fontSize: ["24vw", "12vw"],
-                  lineHeight: "0.8",
-                  fontWeight: 800,
-                  px: [3, 4],
-                  mb: 2,
-                  color: "white",
-                  fontFamily: '"Fira Code"',
-                  textShadow: `0px 0px 21.4900016784668px rgba(0, 128, 1),
-                          0px 0px 32.9800033569336px rgba(0, 128, 0.9),
-                          0px 0px 105.42999267578125px rgba(0, 128, 0.8),
-                          0px 0px 150.8599853515625px rgba(0, 128, 0.7)`,
-                }}
-              >
-                REB00T!
-              </Heading>
-              <Flex>
-                <Box
-                  sx={{
-                    height: "18.42px",
-                    width: "23.61px",
-                    borderBottom: "1px solid",
-                    borderLeft: "1px solid",
-                    borderColor: "rgba(175, 175, 175, 1)",
-                  }}
-                />
-                <Box sx={{ flexGrow: 1 }} />
-                <Box
-                  sx={{
-                    height: "18.42px",
-                    width: "23.61px",
-                    borderBottom: "1px solid",
-                    borderRight: "1px solid",
-                    borderColor: "rgba(175, 175, 175, 1)",
-                  }}
-                />
-              </Flex>
-            </Box>
-            <Flex
-              sx={{
-                alignItems: "center",
-                justifyContent: ["center", `flex-end`],
-              }}
-            >
-              <Flex
-                sx={{
-                  flexDirection: "column",
-                  gap: 3,
-                  fontSize: ["6vw", 3],
-                  textAlign: ["center", "right"],
-                  textTransform: "uppercase",
-                  fontFamily: `"Fira Code Zero", "IBM Plex Mono"`,
-                  maxWidth: "75vw",
-                }}
-              >
-                <Box
-                  sx={{
-                    fontWeight: 800,
-                    maxWidth: ["60vw", "100vw"],
-                    margin: ["auto", 0]
-                  }}
-                >
-                  Apr. 07 2023 to Apr. 09 2023
-                </Box>
-                <Box sx={{ fontWeight: 800 }}><b>Shanghai, China</b> </Box>
-                <Box> Doors open at 6PM; 44-hours long </Box>
-              </Flex>
-            </Flex>
-            <ScrollHint mobile />
-          </Grid>
-          <Flex sx={{flexGrow: 1, alignItems: 'flex-end', mb: 5}}>
-          <ScrollHint mobile={false} /></Flex>
-        </Flex>
-
-      </Grid>
-      <Hackathon />
-      <Layout>
-        <Glossary />
-        <Register />
-      </Layout>
+    <ThemeProvider theme={theme}>
+      <div sx={{ 
+        maxWidth: '960px', 
+        margin: '0 auto', 
+        fontFamily: 'body', 
+        lineHeight: 'body',
+        }}>
+        <header sx={{ 
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          borderBottom: '1px solid',
+          borderColor: 'gray.3',
+          py: 3,
+          }}>
+          <div sx={{ 
+            fontWeight: 'bold',
+            fontSize: 4,
+          }}>
+            Reboot Hackathon
+          </div>
+          <nav sx={{ 
+            display: 'flex',
+            alignItems: 'center',
+          }}>
+            <a href="#about" sx={{ 
+              variant: 'navLink',
+            }}>
+              About
+            </a>
+            <a href="#location" sx={{ 
+              variant: 'navLink',
+            }}>
+              Location
+            </a>
+            <a href="#register" sx={{ 
+              variant: 'navLink',
+            }}>
+              Register
+            </a>
+            <a href="#donate" sx={{ 
+              variant: 'navLink',
+            }}>
+              Donate
+            </a>
+          </nav>
+        </header>
+        <main sx={{ 
+          padding: 3,
+          }}>
+          <section id="about" sx={{ 
+            borderBottom: '1px solid',
+            borderColor: 'gray.3',
+            pb: 4,
+            }}>
+            <h1 sx={{ 
+              variant: 'heading',
+            }}>
+              Reboot Hackathon
+            </h1>
+            <p sx={{ 
+              mt: 3,
+            }}>
+              It is the first in-person hackathon in China since COVID-19.
+            </p>
+            <p sx={{ 
+              mt: 3,
+            }}>
+              Join us for a weekend of hacking, learning, and fun!
+            </p>
+            <p sx={{ 
+              mt: 3,
+            }}>
+              Event Date: April 7th to 9th
+            </p>
+          </section>
+          <section id="location" sx={{ 
+            borderBottom: '1px solid',
+            borderColor: 'gray.3',
+            pb: 4,
+            }}>
+            <h2 sx={{ 
+              variant: 'subheading',
+            }}>
+              Location
+            </h2>
+            <p sx={{ 
+              mt: 3,
+            }}>
+              Microsoft's office at Shanghai Caohejing
+            </p>
+          </section>
+          <section id="register" sx={{ 
+            borderBottom: '1px solid',
+            borderColor: 'gray.3',
+            pb: 4,
+            }}>
+            <h2 sx={{ 
+              variant: 'subheading',
+            }}>
+              Register
+            </h2>
+            <p sx={{ 
+              mt: 3,
+            }}>
+              Register now to secure your spot in the hackathon!
+            </p>
+            <button sx={{ 
+              variant: 'button',
+              mt: 3,
+            }}>
+              Register Now
+            </button>
+          </section>
+          <section id="donate" sx={{ 
+            pb: 4,
+            }}>
+            <h2 sx={{ 
+              variant: 'subheading',
+            }}>
+              Donate
+            </h2>
+            <p sx={{ 
+              mt: 3,
+            }}>
+              Help support the hackathon
+         </p>
+        <p sx={{ 
+          mt: 3,
+        }}>
+          Your donation will go towards funding prizes, food, and supplies for the event.
+        </p>
+        <button sx={{ 
+          variant: 'button',
+          mt: 3,
+        }}>
+          Donate Now
+        </button>
+      </section>
+    </main>
+    <footer sx={{ 
+      borderTop: '1px solid',
+      borderColor: 'gray.3',
+      py: 3,
+      mt: 4,
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      }}>
+      <div>
+        Reboot Hackathon &copy; 2023
+      </div>
+      <div>
+        Made with &hearts; by your name
+      </div>
+    </footer>
+  </div>
+</ThemeProvider>
+              
     </>
   );
 }
