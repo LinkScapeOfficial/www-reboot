affa9e285715cd02fe99301102b433cb2054d519
Thomas Wu <62056970+thomaswcy@users.noreply.github.com>
2023-03-01 20:38:54 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -35,9 +35,9 @@ const Main = () => (
        >
          REB00T
       </Heading>
-      <br />
-      <br />
       <Box sx={{ fontSize: [3, 4], mt: [3, 4], mb: [4, 5] }}>
+        <br />
+        <br />
         <Text>
           Date: April 7th to 9th, 2023
         </Text>
