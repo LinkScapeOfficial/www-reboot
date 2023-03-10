53146dfa01e55ddfa227d2b5edc05a854b4efcfb
Thomas Wu <62056970+thomaswcy@users.noreply.github.com>
2023-03-10 20:08:52 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 import Meta from "@hackclub/meta";
 import Flag from "../components/flag";
 import Head from "next/head";
-import { Heading, Box, Text, Flex, Button, Styled } from "theme-ui";
-import { jsx, Styled, ThemeProvider } from "theme-ui";
+import { Heading, Box, Text, Flex, Button } from "theme-ui";
 
 const Main = () => (
   <>
@@ -13,11 +12,8 @@ const Main = () => (
       description="In Shanghai, this April, 30 teenage hackers will reboot hackathons in China."
       color={`#FF4794`}
     />
-    <Box sx={{ mt: [5, 6], textAlign: "center" }}>
-      <img
-        src="https://cloud-c5ssvb2yg-hack-club-bot.vercel.app/0logo.png"
-        height="200"
-      />
+    <Box sx={{ mt: [5, 6], textAlign: 'center' }}>
+      <img src="https://cloud-c5ssvb2yg-hack-club-bot.vercel.app/0logo.png" height="200" />
       <br />
       <br />
       <br />
@@ -36,69 +32,51 @@ const Main = () => (
                   0px 0px 105.42999267578125px rgba(0, 128, 0.8),
                   0px 0px 150.8599853515625px rgba(0, 128, 0.7)`,
         }}
-      >
-        REB00T
+       >
+         REB00T
       </Heading>
       <Box sx={{ fontSize: [3, 4], mt: [3, 4], mb: [4, 5] }}>
-        <Text>Date: April 7th to 9th, 2023</Text>
+        <Text>
+          Date: April 7th to 9th, 2023
+        </Text>
         <br />
-        <Text>Location: Microsoft Reactor Shanghai (Caohejing)</Text>
+        <Text>
+          Location: Microsoft Reactor Shanghai (Caohejing)
+        </Text>
       </Box>
-      <Flex sx={{ justifyContent: "center" }}>
+      <Flex sx={{ justifyContent: 'center' }}>
         <Box sx={{ mr: 3 }}>
           <a href="https://airtable.com/shrUb9NI0tm8hv8at">
-            <Button
-              sx={{
-                fontSize: [2, 3],
-                px: [4, 5],
-                py: [2, 3],
-                bg: "primary",
-                color: "background",
-                borderRadius: "default",
-                cursor: "pointer",
-                "&:hover": { bg: "secondary" },
-              }}
-            >
+            <Button sx={{ fontSize: [2, 3], px: [4, 5], py: [2, 3], bg: 'primary', color: 'background', borderRadius: 'default', cursor: 'pointer', '&:hover': { bg: 'secondary' } }}>
               Register
-          </Button>
+            </Button>
           </a>
         </Box>
         <Box>
           <a href="https://bank.hackclub.com/donations/start/reboot-2023">
-            <Button
-              sx={{
-                fontSize: [2, 3],
-                px: [4, 5],
-                py: [2, 3],
-                bg: "secondary",
-                color: "background",
-                borderRadius: "default",
-                cursor: "pointer",
-                "&:hover": { bg: "primary" },
-              }}
-            >
+            <Button sx={{ fontSize: [2, 3], px: [4, 5], py: [2, 3], bg: 'secondary', color: 'background', borderRadius: 'default', cursor: 'pointer', '&:hover': { bg: 'primary' } }}>
               Donate
-          </Button>
+            </Button>
           </a>
         </Box>
       </Flex>
     </Box>
-    <div sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+    <div sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <Styled.h1>Event Schedule</Styled.h1>
       <table
         sx={{
-          borderCollapse: "collapse",
-          width: "100%",
-          maxWidth: "600px",
-          textAlign: "center",
+          borderCollapse: 'collapse',
+          width: '100%',
+          maxWidth: '600px',
+          textAlign: 'center',
         }}
       >
         <thead>
           <tr
             sx={{
-              backgroundColor: "primary",
-              color: "background",
-              borderRadius: "10px",
+              backgroundColor: 'primary',
+              color: 'background',
+              borderRadius: '10px',
             }}
           >
             <th sx={{ py: 2 }}>Time</th>
@@ -149,20 +127,4 @@ const Main = () => (
   </>
 );
 
-const theme = {
-  colors: {
-    text: "#000",
-    background: "#fff",
-    primary: "#07c",
-  },
-};
-
-function MyApp({ Component, pageProps }) {
-  return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
-}
-
-export default MyApp;
+export default Main;
